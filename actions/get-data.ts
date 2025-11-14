b69d0e1c934521019169756f9b5d8f4a22cdeb7e
sirdav1d <ddavid.diniz@gmail.com>
2025-11-14 12:32:22 -0300

    fix: add try catch

--- actions/get-data.ts
+++ actions/get-data.ts
@@ -3,16 +3,24 @@
 'use server';
 
 export async function getData() {
-	const resp = await fetch(
-		'https://rjnfrwp904.execute-api.us-east-1.amazonaws.com/envhospital/user',
-		{
-			headers: {
-				Authorization: `Bearer hw9X6oBH-JNynNJIuKEnP2B0q4zrPWhpPzoVZ8AsR`,
+	try {
+		const resp = await fetch(
+			'https://rjnfrwp904.execute-api.us-east-1.amazonaws.com/envhospital/user',
+			{
+				headers: {
+					Authorization: `Bearer hw9X6oBH-JNynNJIuKEnP2B0q4zrPWhpPzoVZ8AsR`,
+				},
 			},
-		},
-	);
+		);
+		const data = await resp.json();
 
-	const data = await resp.json();
+		if (!data) {
+			console.log(resp);
+			return 'data not found';
+		}
 
-	return data;
+		return data;
+	} catch (error) {
+		return error;
+	}
 }
